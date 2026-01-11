89cc08af7e271db06e6aef29f213cf66fbb91950
Dinyar <migranovdiniar@gmail.com>
2026-01-11 20:24:37 +0300

    попроваил фронт для аутентификации

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Button, Container, Row } from 'react-bootstrap'
 import './LandingPage.css'
 
 function LandingPage() {
@@ -11,6 +11,18 @@ function LandingPage() {
                     <div>
                         <h1 className='title'>Welcome to Notes App</h1>
                         <p className='subtitle'>One Safe place for all your notes</p>
+                    </div>
+                    <div className='buttonContainer'>
+                        <a href='/login'>
+                            <Button size='lg' className='landingbutton'>
+                                Login
+                            </Button>
+                        </a>
+                        <a href='/register'>
+                            <Button variant='outline-primary' size='lg' className='landingbutton'>
+                                Sign Up
+                            </Button>
+                        </a>
                     </div>
                 </div>
             </Row>
