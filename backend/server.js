e42ca96935ef34732a53af65c734faa0c07bd86e
Dinyar <migranovdiniar@gmail.com>
2026-01-12 05:51:58 +0300

    connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
