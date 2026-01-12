45720ca8f85df192fffc0257e73aa31adeca67f6
Dinyar <migranovdiniar@gmail.com>
2026-01-12 07:25:49 +0300

    bcrypt

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,10 +1,36 @@
-const registerUser = async (req, res) => {
+const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
+
+
+const registerUser = asyncHandler (async (req, res) => {
     const { name, email, password, pic } = req.body;
 
-    res.json({
+    const userExists = await User.findOne({ email });
+
+    if(userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    const user = await User.create({
         name,
         email,
-    })
-}
+        password,
+        pic,
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isNadmin: user.isAdmin,
+            pic: user.pic,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Error occurred during user creation");
+    }
+});
 
 module.exports = { registerUser };
